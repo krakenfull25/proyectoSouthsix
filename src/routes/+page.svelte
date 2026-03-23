378ebe74ca5079e407277c17df4d619eae9e91b6
Ignacio <idurmes337@g.educaand.es>
2026-03-23 14:25:25 +0100

    Añadir componente header a root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
+<script>
+    import Header from "$lib/Components/Header.svelte";
+</script>
+<Header/>
+
 <h1>Plantilla de proyecto For-Him</h1>
 <p>Creada por el goat ignduu05</p>
-<img src="https://media.tenor.com/BuSEbkm9aAIAAAAi/hi-otag.gif"/>
+<img src="https://media.tenor.com/BuSEbkm9aAIAAAAi/hi-otag.gif" alt="meme de ejemplo"/>
+
+<style lang="scss">
+    :global(body, html) {
+    margin: 0;
+    padding: 0;
+  }
+  :global(*){
+    box-sizing: border-box;
+  }
+</style>
