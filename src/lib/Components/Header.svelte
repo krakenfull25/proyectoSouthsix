a97dc1b702d8bba22de49732ec604f5f156bb28a
Ignacio <idurmes337@g.educaand.es>
2026-03-23 14:25:10 +0100

    Crear prototipo componente header

---
+++ src/lib/Components/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+  import hamburgerIcon from '$lib/assets/header-icons/hamburger-icon.svg';
+  import searchIcon from '$lib/assets/header-icons/search-icon.svg';
+  import shoppingCart from '$lib/assets/header-icons/shopping-cart.svg';
+  import userProfile from '$lib/assets/header-icons/user-profile.svg';
+</script>
+
+<header>
+    <div class="content">
+        <img src={hamburgerIcon} alt="Icono hamburger">
+        <img src={searchIcon} alt="Icono busqueda">
+        <h2>EL LOGO AQUI</h2>
+        <img src={shoppingCart} alt="Icono carrito">
+        <img src={userProfile} alt="Icono perfil">
+    </div>
+</header>
+
+<style lang="scss">
+    header {
+        background-color: #2D3E40;
+
+        > .content {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            gap: 26px;
+            justify-content: space-between;
+            padding: 30px 20px;
+        }
+    }
+</style>
