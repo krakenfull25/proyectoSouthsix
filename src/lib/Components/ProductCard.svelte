d144525e4fa1b05e91de087476bf9eba7e894cfc
Ignacio <idurmes337@g.educaand.es>
2026-03-24 12:36:06 +0100

    Añadir componente Card (añadir CSS correspondiente)

---
+++ src/lib/Components/ProductCard.svelte
@@ -0,0 +1,25 @@
+<!--
+Card de producto que se muestra en la página home.
+-->
+
+<div class="card">
+    <div class="image">
+        <p>Aqui va la imagen del producto que sea, más tarde se pasará por props.</p>
+    </div>
+    <div class="card-data">
+        <p>Nombre de producto</p>
+        <p>Tipo de producto</p>
+        <div class="price">
+            <p>Precio (€)</p>
+            <p>8,99</p>
+        </div>
+        <div class="buttons">
+            <p>BOTON ME GUSTA AQUI</p>
+            <p>BOTON CARRITO AQUI</p>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+
+</style>
