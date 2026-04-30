f8d6c5038b2acd9c6c68d44c66643dea3347a0fc
igndu05 <idurmes337@g.educaand.es>
2026-04-30 11:52:00 +0200

    Correccion estilos CSS

--- src/lib/Components/ImageDisplay.svelte
+++ src/lib/Components/ImageDisplay.svelte
@@ -15,66 +15,89 @@ Este componente se utiliza en product-detail, se muestra las imagenes del produc
 			{#each producto.imagenes as imagen}
 				<img src={imagen} alt="imagen"/>
 			{/each}
+			{#each producto.imagenes as imagen}
+				<img src={imagen} alt="imagen"/>
+			{/each}
 		</div>
 	</div>
 </div>
 
 <style lang="scss">
     .image {
-        margin: 30px 18px;
+    margin: 30px 18px;
 
-        > h2 {
-            border-bottom: 1px solid black;
+    > h2 {
+        border-bottom: 1px solid black;
+    }
+
+    .image-display {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        gap: 36px;
+
+        .main-image {
+            border: 1px solid black;
+            width: 500px;
+            height: 500px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            > img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                display: block;
+            }
         }
 
-		.image-display {
-			display: flex;
-			flex-direction: column;
-			gap: 36px;
+        .other-images {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 48px;
+            height: 150px;
 
-			.main-image {
-				width: 100%;
-				border: 1px solid black;
+            > img {
+                width: 150px;
+                height: 150px;
+                border: 1px solid black;
+                margin: 0;
+            }
+        }
+    }
+
+    
+    @media (max-width: 1024px) {
+        .image-display {
+            flex-direction: column;
+
+            .main-image {
+                width: 100%;
+                
+            }
+
+			.other-images {
+				grid-template-columns: repeat(4,1fr);
+				height: auto;
 
 				> img {
-					width: 100%;
+					width:100%;
 					height: 100%;
 					object-fit: cover;
 					display: block;
 				}
 			}
+        }
+    }
+
+	@media (max-width: 600px) {
+		.image-display {
 
 			.other-images {
-				display: grid;
 				grid-template-columns: repeat(3,1fr);
-				gap: 20px;
-				
-				> img {
-					position: relative;
-					width: 150px;
-					height: 150px;
-					border: 1px solid black;
-					margin: 0;
-				}
 			}
-		}
-    }
-    
-	@media (min-width:1024px) {
-		.image {
-			> .image-display {
-				flex-direction: row;
-				gap: 50px;
-
-				.other-images {
-				display: grid;
-				> img {
-					
-					border: 1px solid black;
-					margin: 0;
-				}
-			}
-			}
-		}
+        }
 	}
+}
 </style>
